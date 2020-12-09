54754c29afb5f6b1a83992fbc01ca057392e0a69
Auronen <45665432+auronen@users.noreply.github.com>
2020-12-10 00:56:58 +0100

    Update DIA_SFB_1000_Senyan.d
    Korektura

--- content/Story/Missions/DIA_SFB_1000_Senyan.d
+++ content/Story/Missions/DIA_SFB_1000_Senyan.d
@@ -35,7 +35,7 @@ instance DIA_Senyan_Hello(C_INFO)
 	permanent = 0;
 //	description = "You're from the Free Mine, right?";
 //	description = "Du kommst aus der Freien Mine, richtig?";
-	description = "Ty jsi ze Svobodného dolu, že jo?";
+	description = "Ty jsi ze Svobodného dolu, že?";
 };                       
 
 func int DIA_Senyan_Hello_Condition()
@@ -47,10 +47,10 @@ func void DIA_Senyan_Hello_Info()
 { 
 //	AI_Output(other,self,"DIA_Senyan_Hello_15_00"); //You're from the Free Mine, right?
 //	AI_Output(other,self,"DIA_Senyan_Hello_15_00"); //Du kommst aus der Freien Mine, richtig?
-	AI_Output(other,self,"DIA_Senyan_Hello_15_00"); //Ty jsi ze Svobodného dolu, že jo?
+	AI_Output(other,self,"DIA_Senyan_Hello_15_00"); //Ty jsi ze Svobodného dolu, že?
 //	AI_Output(self,other,"DIA_Senyan_Hello_01_01"); //Right. How did you find that out?
 //	AI_Output(self,other,"DIA_Senyan_Hello_01_01"); //Richtig. Wie hast du das bloß rausgefunden?
-	AI_Output(self,other,"DIA_Senyan_Hello_01_01"); //Ano. Jak jsi na to přišel?
+	AI_Output(self,other,"DIA_Senyan_Hello_01_01"); //Ano. A jak jsi na to přišel?
 	AI_StopProcessInfos(self);
 };
 
@@ -67,7 +67,7 @@ instance DIA_Senyan_JustTalk(C_INFO)
 	permanent = 1;
 //	description = "I just want to talk to you.";
 //	description = "Ich will nur mit dir reden.";
-	description = "Chci s tebou mluvit.";
+	description = "Chci s tebou jen mluvit.";
 };                       
 
 func int DIA_Senyan_JustTalk_Condition()
@@ -82,7 +82,7 @@ func void DIA_Senyan_JustTalk_Info()
 { 
 //	AI_Output(other,self,"DIA_Senyan_JustTalk_15_00"); //I just want to talk to you.
 //	AI_Output(other,self,"DIA_Senyan_JustTalk_15_00"); //Ich will nur mit dir reden.
-	AI_Output(other,self,"DIA_Senyan_JustTalk_15_00"); //Chci s tebou mluvit.
+	AI_Output(other,self,"DIA_Senyan_JustTalk_15_00"); //Chci s tebou jen mluvit.
 //	AI_Output(self,other,"DIA_Senyan_JustTalk_01_01"); //I'm not here to talk.
 //	AI_Output(self,other,"DIA_Senyan_JustTalk_01_01"); //Ich bin nicht zum Reden hier.
 	AI_Output(self,other,"DIA_Senyan_JustTalk_01_01"); //Nejsem tu kvůli mluvení.

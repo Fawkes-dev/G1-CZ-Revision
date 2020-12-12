5845c111386fc217475caf69dde61f43a4a196d5
Auronen <45665432+auronen@users.noreply.github.com>
2020-12-12 17:13:17 +0100

    Update DIA_SLD_761_Soeldner.d
    Korektura

--- content/Story/Missions/DIA_SLD_761_Soeldner.d
+++ content/Story/Missions/DIA_SLD_761_Soeldner.d
@@ -37,7 +37,7 @@ instance DIA_SLD_761_Soeldner_EXIT_1(C_INFO)
 	permanent = 0;
 //	description = "I've still got some business to do.";
 //	description = "Ich hab' noch was zu erledigen.";
-	description = "Dělám pořád tu samou práci.";
+	description = "Mám tu přád nějkaou práci.";
 };
 
 func int DIA_SLD_761_Soeldner_EXIT_1_CONDITION()
@@ -49,7 +49,7 @@ func void DIA_SLD_761_Soeldner_EXIT_1_INFO()
 {
 //	AI_Output(other,self,"DIA_SLD_761_Soeldner_EXIT_1_INFO_15_01"); //I've still got some business to do.
 //	AI_Output(other,self,"DIA_SLD_761_Soeldner_EXIT_1_INFO_15_01"); //Ich hab' noch was zu erledigen.
-	AI_Output(other,self,"DIA_SLD_761_Soeldner_EXIT_1_INFO_15_01"); //Dělám pořád tu samou práci.
+	AI_Output(other,self,"DIA_SLD_761_Soeldner_EXIT_1_INFO_15_01"); //Mám tu přád nějkaou práci.
 //	AI_Output(self,other,"DIA_SLD_761_Soeldner_EXIT_1_INFO_03_02");//That's what I think as well.
 //	AI_Output(self,other,"DIA_SLD_761_Soeldner_EXIT_1_INFO_03_02");//Das denke ich auch.
 	AI_Output(self,other,"DIA_SLD_761_Soeldner_EXIT_1_INFO_03_02");//Taky si to myslím.
@@ -116,7 +116,7 @@ func void DIA_SLD_761_Soeldner_UMSEHEN_INFO()
 	AI_Output(other,self,"DIA_SLD_761_Soeldner_UMSEHEN_INFO_15_01"); //Jen jsem se chtěl porozhlédnout po táboře.
 //	AI_Output(self,other,"DIA_SLD_761_Soeldner_UMSEHEN_INFO_03_02");//Then you've come to the wrong place, because this is the mine entrance and you're not admitted here.
 //	AI_Output(self,other,"DIA_SLD_761_Soeldner_UMSEHEN_INFO_03_02");//Dann bist du hier wohl falsch, hier ist der Mineneingang, und der ist tabu für dich.
-	AI_Output(self,other,"DIA_SLD_761_Soeldner_UMSEHEN_INFO_03_02");//Pak jsi přišel na nesprávné místo, protože tohle je vchod do dolu, kam tě nepustíme.
+	AI_Output(self,other,"DIA_SLD_761_Soeldner_UMSEHEN_INFO_03_02");//Pak jsi přišel na nesprávné místo, protože tohle je vchod do dolu, a tam tě nepustíme.
 };
 
 //----------------------- Will nur mal in die Mine ------------------
@@ -130,7 +130,7 @@ instance DIA_SLD_761_Soeldner_INMINE(C_INFO)
 	permanent = 0;
 //	description = "I just wanted to see the mine.";
 //	description = "Ich wollte mir nur mal die Mine ansehen.";
-	description = "Chtěl jsem vidět důl.";
+	description = "Jen jsem si chtěl ten důl.";
 };
 
 func int DIA_SLD_761_Soeldner_INMINE_CONDITION()
@@ -142,7 +142,7 @@ func void DIA_SLD_761_Soeldner_INMINE_INFO()
 {
 //	AI_Output(other,self,"DIA_SLD_761_Soeldner_INMINE_INFO_15_01");//I just wanted to see the mine.
 //	AI_Output(other,self,"DIA_SLD_761_Soeldner_INMINE_INFO_15_01");//Ich wollte mir nur mal die Mine ansehen.
-	AI_Output(other,self,"DIA_SLD_761_Soeldner_INMINE_INFO_15_01");//Chtěl jsem vidět důl.
+	AI_Output(other,self,"DIA_SLD_761_Soeldner_INMINE_INFO_15_01");//Jen jsem si chtěl ten důl.
 //	AI_Output(self,other,"DIA_SLD_761_Soeldner_INMINE_INFO_03_02");//Suppose you can see that the mine is locked, so there's absolutely nothing for you to see.
 //	AI_Output(self,other,"DIA_SLD_761_Soeldner_INMINE_INFO_03_02");//Du siehst doch, dass die Mine abgeschlossen ist, also hier gibt´s nichts zu sehen.
 	AI_Output(self,other,"DIA_SLD_761_Soeldner_INMINE_INFO_03_02");//Snad vidíš, že důl je zamčený, takže tu není vůbec nic k vidění.
@@ -178,13 +178,13 @@ func void DIA_SLD_761_Soeldner_Bribe_INFO()
 	AI_Output(other,self,"DIA_SLD_761_Soeldner_Bribe_INFO_15_01");//Nemohlo by se s tím něco udělat?
 //	AI_Output(self,other,"DIA_SLD_761_Soeldner_Bribe_INFO_03_02");//What should there be to do about it?
 //	AI_Output(self,other,"DIA_SLD_761_Soeldner_Bribe_INFO_03_02");//Was sollte man da machen können?
-	AI_Output(self,other,"DIA_SLD_761_Soeldner_Bribe_INFO_03_02");//Co bysme s tím měli udělat?
+	AI_Output(self,other,"DIA_SLD_761_Soeldner_Bribe_INFO_03_02");//Co bychom s tím měli udělat?
 //	AI_Output(self,other,"DIA_SLD_761_Soeldner_Bribe_INFO_03_03");//One moment, if you're trying to bribe me, you have the wrong man here.
 //	AI_Output(self,other,"DIA_SLD_761_Soeldner_Bribe_INFO_03_03");//Moment mal, wenn du versucht, mich zu bestechen, dann bist du hier an der falschen Adresse.
 	AI_Output(self,other,"DIA_SLD_761_Soeldner_Bribe_INFO_03_03");//Okamžik, jestli se mě pokoušíš podplatit, tak to jsi narazil na toho nesprávného.
 //	AI_Output(other,self,"DIA_SLD_761_Soeldner_Bribe_INFO_15_04");//No, I wasn't trying to bribe anybody, I just thought that...
 //	AI_Output(other,self,"DIA_SLD_761_Soeldner_Bribe_INFO_15_04");//Nein, ich wollte niemanden bestechen, ich dachte nur..
-	AI_Output(other,self,"DIA_SLD_761_Soeldner_Bribe_INFO_15_04");//Ne, nepokoušel jsem se nikoho podplatit, jenom jsem myslel...
+	AI_Output(other,self,"DIA_SLD_761_Soeldner_Bribe_INFO_15_04");//Ne, nepokoušel jsem se nikoho podplatit, jen jsem myslel...
 //	AI_Output(other,self,"DIA_SLD_761_Soeldner_Bribe_INFO_15_05");//I guess I'd best be going.
 //	AI_Output(other,self,"DIA_SLD_761_Soeldner_Bribe_INFO_15_05");//Ich glaub', ich geh' dann besser.
 	AI_Output(other,self,"DIA_SLD_761_Soeldner_Bribe_INFO_15_05");//Já myslím, že bys měl raději jít pryč.

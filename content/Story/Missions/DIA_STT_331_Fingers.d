719cc52ecec731651d4a46b277436cbacb9939cb
Auronen <45665432+auronen@users.noreply.github.com>
2020-12-14 18:47:49 +0100

    Update DIA_STT_331_Fingers.d
    Korektura

--- content/Story/Missions/DIA_STT_331_Fingers.d
+++ content/Story/Missions/DIA_STT_331_Fingers.d
@@ -101,16 +101,16 @@ func void DIA_Fingers_BecomeShadow_TeachMe()
 	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_03"); //To jsi měl říci rovnou.
 //	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_04"); //If you wanna join the Camp, it's an advantage to be a good fighter or thief.
 //	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_04"); //Wenn du im Lager aufgenommen werden willst, hilft es, wenn du ein geschickter Kämpfer oder Dieb bist.
-	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_04"); //Jestli se chceš přidat k táboru, pak je výhodné být dobrý bojovník nebo zloděj.
+	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_04"); //Jestli se chceš přidat k táboru, pak je dobré být zručný bojovník nebo zloděj.
 //	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_05"); //If the influential Shadows in this camp test you, they'll give you tasks you'd better not get caught doing.
 //	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_05"); //Wenn die einflussreichen Schatten in diesem Lager dich auf die Probe stellen, werden sie dir Aufträge geben, bei denen du dich besser nicht erwischen lässt.
-	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_05"); //Jestli tě budou tady v táboře zkoušet nezávislé Stíny, dají ti takové úkoly, které nebudeš nejspíš schopný splnit.
+	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_05"); //Jestli tě tady v táboře budou zkoušet vlivní Stínové, dají ti takové úkoly, při kterých se raději nenechej chytit.
 //	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_06"); //That's why the way of the thief would be a good start to you.
 //	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_06"); //Deswegen ist für den Anfang der Weg des Diebes für dich vielversprechender.
-	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_06"); //Proto by ses pro začátek mohl stát zlodějem.
+	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_06"); //Proto je pro tebe slibnější cesta zloděje.
 //	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_07"); //Incidentally, I'm the best thief in the whole of the Old Camp.
 //	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_07"); //Wie es der Zufall so will, bin ich der beste Dieb im ganzen Alten Lager.
-	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_07"); //Mimochodem, já jsem nejlepší zloděj z celého Starého tábora.
+	AI_Output(self,other,"DIA_Fingers_BecomeShadow_TeachMe_05_07"); //Jakoby náhodou, já jsem ten nejlepší zloděj v celém Starém táboře.
 	Fingers_CanTeach = TRUE;
 	Log_CreateTopic(GE_TeacherOC,LOG_NOTE);
 //	B_LogEntry(GE_TeacherOC,"Fingers can teach me the skills of PICKPOCKETING and how to OPEN LOCKS");
@@ -171,10 +171,10 @@ func void DIA_Fingers_Lehrer_Schleichen()
 	AI_Output(other,self,"DIA_Fingers_Lehrer_Schleichen_15_00"); //Chci se naučit plížit.
 //	AI_Output(self,other,"DIA_Fingers_Lehrer_Schleichen_05_01"); //I believe - no false modesty - that I'm one of the best thieves they ever chucked into this colony.
 //	AI_Output(self,other,"DIA_Fingers_Lehrer_Schleichen_05_01"); //Ich glaube - ohne falsche Bescheidenheit - ich bin einer der besten Diebe, die sie in die Kolonie geworfen haben.
-	AI_Output(self,other,"DIA_Fingers_Lehrer_Schleichen_05_01"); //Věřím, a to bez nadsázky, že jsem jeden z nejlepších zlodějů, jaké kdy tahle kolonie spatřila.
+	AI_Output(self,other,"DIA_Fingers_Lehrer_Schleichen_05_01"); //Věřím, a to bez nadsázky, že jsem jeden z nejlepších zlodějů, kterého do téhle Kolone shodili.
 //	AI_Output(self,other,"DIA_Fingers_Lehrer_Schleichen_05_02"); //But when it comes to teaching how to sneak around, Cavalorn is the better man for you!
 //	AI_Output(self,other,"DIA_Fingers_Lehrer_Schleichen_05_02"); //Aber wenn es darum geht, zu lernen, wie man sich leise bewegt, ist Cavalorn der bessere Mann für dich!
-	AI_Output(self,other,"DIA_Fingers_Lehrer_Schleichen_05_02"); //Jestli se chceš ale naučit plížit, Cavalorn bude lepší učitel než já!
+	AI_Output(self,other,"DIA_Fingers_Lehrer_Schleichen_05_02"); //Ale jestli se chceš naučit plížit, je Cavalorn lepší volba než já!
 	Fingers_Wherecavalorn = TRUE;
 };
 
@@ -194,7 +194,7 @@ func void DIA_Fingers_Lehrer_Lockpick()
 		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick_05_02"); //Především se musíš ujistit, že se tvůj paklíč nezlomí.
 //		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick_05_03"); //If you're a little bit more patient in future, you'll see that you won't need that many lockpicks any more!
 //		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick_05_03"); //Wenn du in Zukunft mit weniger Gewalt an die Sache rangehst, wirst du merken, dass du weniger Dietriche verbrauchst!
-		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick_05_03"); //Když budeš v budoucnu trochu trpělivější, uvidíš, že nebudeš potřebovat tolik paklíčů!
+		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick_05_03"); //Pokud budeš budeš k zámkům přistupovat s menší silou, zjistíš, že nepotřebuješ tolik paklíčů.
 	};
 };
 
@@ -208,13 +208,13 @@ func void DIA_Fingers_Lehrer_Lockpick2()
 	{
 //		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick2_05_01"); //Only when you've gained some experience can you tell when a lockpick is going to break. It makes a different sound!
 //		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick2_05_01"); //Nur wer schon einige Übung hat, merkt genau, wann ihm ein Dietrich abbricht und wann nicht. Du erkennst es am Geräusch!
-		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick2_05_01"); //Jedině když získáš nějaké zkušenosti, budeš moci říci, jestli se paklíč zlomí. Vydává jiný zvuk!
+		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick2_05_01"); //Až když získáš nějaké zkušenosti odhadneš, kdy se poklíč zlomí. Poznáš to podle zvuku! 
 //		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick2_05_02"); //I think you have it. Learn to listen for the gentle clicking of the lock and you won't lose so many lockpicks in future.
 //		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick2_05_02"); //Ich denke, du bist jetzt so weit - lerne, auf das leise Klicken des Schlosses zu achten, und du wirst nur noch selten das Pech haben, einen Dietrich zu verlieren.
-		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick2_05_02"); //Myslím, že to máš. Nauč se poslouchat to jemné cvaknutí zámku a pak už nebudeš přicházet o tolik paklíčů.
+		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick2_05_02"); //Myslím, že to máš. Nauč se dávat pozor na jemné cvaknutí v zámku, pak už nebudeš přicházet o tolik paklíčů.
 //		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick2_05_03"); //An expert in this field stands a good chance of opening a trunk without breaking his lockpick.
 //		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick2_05_03"); //Ein Meister des Fachs hat eine gute Chance, eine Truhe zu öffnen, ohne dass sein Dietrich überhaupt abbricht.
-		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick2_05_03"); //Odborník v tomto směru má velkou šanci, že otevře zámek, aniž by zlomil svůj paklíč.
+		AI_Output(self,other,"DIA_Fingers_Lehrer_Lockpick2_05_03"); //Odborník v tomto oboru má velkou šanci, že odemkne turhlici, aniž by svůj paklíč zlomil.
 	};
 };
 
@@ -232,10 +232,10 @@ func void DIA_Fingers_Lehrer_Pickpocket()
 			AI_Output(self,other,"DIA_Fingers_Lehrer_PICKPOCKET_05_01"); //Takže chceš pár lidem odlehčit o jejich majetek? Dobrá teda.
 //			AI_Output(self,other,"DIA_Fingers_Lehrer_PICKPOCKET_05_02"); //I'll teach you how to improve your skills, but it's still very likely that you'll get caught.
 //			AI_Output(self,other,"DIA_Fingers_Lehrer_PICKPOCKET_05_02"); //Ich werde dir beibringen, wie du besser wirst, aber es besteht immer noch eine Chance, dass du erwischt wirst.
-			AI_Output(self,other,"DIA_Fingers_Lehrer_PICKPOCKET_05_02"); //Naučím tě, jak zlepšit své umění, ale přesto bude dost pravděpodobné, že tě přitom chytnou.
+			AI_Output(self,other,"DIA_Fingers_Lehrer_PICKPOCKET_05_02"); //Naučím tě, jak se zlepšit, ale přesto bude dost pravděpodobné, že tě přitom chytnou.
 //			AI_Output(self,other,"DIA_Fingers_Lehrer_PICKPOCKET_05_03"); //You should only take the risk if nobody but your victim is around.
 //			AI_Output(self,other,"DIA_Fingers_Lehrer_PICKPOCKET_05_03"); //Das Risiko solltest du nur eingehen, wenn außer deinem Opfer niemand sonst da ist.
-			AI_Output(self,other,"DIA_Fingers_Lehrer_PICKPOCKET_05_03"); //Měl bys to zkoušet pouze tehdy, až nebude kolem nikdo, než ty a tvoje oběť.
+			AI_Output(self,other,"DIA_Fingers_Lehrer_PICKPOCKET_05_03"); //Měl by ses o to pokoušet pouze tehdy, až nebude kolem nikdo jiný než tvá oběť.
 //			AI_Output(self,other,"DIA_Fingers_Lehrer_PICKPOCKET_05_04"); //Only an expert can take things from people's pockets without them noticing!
 //			AI_Output(self,other,"DIA_Fingers_Lehrer_PICKPOCKET_05_04"); //Erst als Meister wirst du in der Lage sein, jemandem etwas aus der Tasche zu ziehen, ohne dass er es merkt!
 			AI_Output(self,other,"DIA_Fingers_Lehrer_PICKPOCKET_05_04"); //Jedině odborník dokáže vzít lidem věci z kapes, aniž by si toho všimli!
@@ -259,7 +259,7 @@ func void DIA_Fingers_Lehrer_Pickpocket2()
 	{ 
 //		AI_Output(self,other,"DIA_Fingers_Lehrer_Pickpocket2_05_01"); //I think you've progressed far enough to be taught the final tricks of the trade.
 //		AI_Output(self,other,"DIA_Fingers_Lehrer_Pickpocket2_05_01"); //Ich denke, du bist gut genug, um in die letzten Geheimnisse eingeweiht zu werden.
-		AI_Output(self,other,"DIA_Fingers_Lehrer_Pickpocket2_05_01"); //Myslím, že jsi pokročil tak daleko, že tě můžu naučit poslední triky tohohle řemesla.
+		AI_Output(self,other,"DIA_Fingers_Lehrer_Pickpocket2_05_01"); //Myslím, že jsi dostatečně dobrý na to, abych tě zasvětil do posledních tajemtví řemesla.
 //		AI_Output(self,other,"DIA_Fingers_Lehrer_Pickpocket2_05_02"); //But you should know that even the best can get caught sometimes.
 //		AI_Output(self,other,"DIA_Fingers_Lehrer_Pickpocket2_05_02"); //Du solltest aber wissen, dass es selbst den Besten manchmal passiert, dass sie bemerkt werden.
 		AI_Output(self,other,"DIA_Fingers_Lehrer_Pickpocket2_05_02"); //Měl bys ale vědět, že i ten nejlepší může být někdy přistižen.
@@ -305,14 +305,14 @@ func void DIA_Fingers_WhereCavalorn_Info()
 	AI_Output(other,self,"DIA_Fingers_WhereCavalorn_15_00"); //Kde najdu Cavalorna?
 //	AI_Output(self,other,"DIA_Fingers_WhereCavalorn_05_01"); //He's hunting out in the wild. You'll find him at the path to the New Camp. Westwards of the Old Camp there's a wide canyon.
 //	AI_Output(self,other,"DIA_Fingers_WhereCavalorn_05_01"); //Er jagt in der Wildnis. Du findest ihn auf dem Weg zum Neuen Lager. Westlich des Alten Lagers beginnt eine breite Schlucht.
-	AI_Output(self,other,"DIA_Fingers_WhereCavalorn_05_01"); //Právě je na lovu v divočině. Najdeš ho po cestě do Nového tábora. Západně od Starého tábora je široký kaňon.
+	AI_Output(self,other,"DIA_Fingers_WhereCavalorn_05_01"); //Loví v divočině. Najdeš ho po cestě do Nového tábora. Západně od Starého tábora je široký kaňon.
 //	AI_Output(self,other,"DIA_Fingers_WhereCavalorn_05_02"); //In this canyon there's an old woodcutters' hut. You'll find him there.
 //	AI_Output(self,other,"DIA_Fingers_WhereCavalorn_05_02"); //In dieser Schlucht steht eine alte Holzfällerhütte. Dort findest du ihn.
 	AI_Output(self,other,"DIA_Fingers_WhereCavalorn_05_02"); //V tom kaňonu je stará dřevorubecká chata. Najdeš ho v ní.
 
 //	B_LogEntry(GE_TeacherOW,"Cavalorn can teach me to sneak. His hut is in a canyon to the west of the Old Camp, in the direction of the New Camp.");
 //	B_LogEntry(GE_TeacherOW,"Cavalorn kann mir Schleichen beibringen. Seine Hütte steht in einer Schlucht westlich des Alten Lagers, auf dem Weg zum Neuen Lager.");
-	B_LogEntry(GE_TeacherOW,"Cavalorm mě může naučit plížení. Jeho chatrč stojí na západ od Starého tábora, směrem k Novému táboru.");
+	B_LogEntry(GE_TeacherOW,"Cavalorm mě může naučit PLÍŽENÍ. Jeho chatrč stojí v kaňonu na západ od Starého tábora, směrem k Novému táboru.");
 };
 
 // **************************************************
@@ -355,7 +355,7 @@ func void DIA_Fingers_Learnt_Info()
 		AI_Output(self,other,"DIA_Fingers_Learnt_05_01"); //Ano. Naučil ses něco, díky čemu budeš pro nás docela užitečný.
 //		AI_Output(self,other,"DIA_Fingers_Learnt_05_02"); //We can make use of skilled thieves in this camp here. You'll have my vote.
 //		AI_Output(self,other,"DIA_Fingers_Learnt_05_02"); //Geschickte Diebe können wir hier im Lager brauchen. Meine Stimme ist dir sicher.
-		AI_Output(self,other,"DIA_Fingers_Learnt_05_02"); //Potřebujeme tady v táboře zkušené zloděje. Dostaneš můj hlas.
+		AI_Output(self,other,"DIA_Fingers_Learnt_05_02"); //Zkušené zloděje v táboře potřebujeme. Dostaneš můj hlas.
 		Fingers_Learnt = LOG_SUCCESS;
 
 		B_GiveXP(XP_Fingerstrain);
@@ -370,10 +370,10 @@ func void DIA_Fingers_Learnt_Info()
 		AI_Output(self,other,"DIA_Fingers_Learnt_05_03"); //Zaleží to na...
 //		AI_Output(self,other,"DIA_Fingers_Learnt_05_04"); //If you demonstrate that you've learned something as a thief, we could make use of you!
 //		AI_Output(self,other,"DIA_Fingers_Learnt_05_04"); //Wenn du zeigst, dass du als Dieb was gelernt hast, könnten wir dich schon brauchen!
-		AI_Output(self,other,"DIA_Fingers_Learnt_05_04"); //Jestli dokážeš, že ses naučil něco ze zlodějského řemesla, mohl bys být pro nás užitečný!
+		AI_Output(self,other,"DIA_Fingers_Learnt_05_04"); //Jestli dokážeš, že ses naučil něco ze zlodějského řemesla, mohli bychom tě potřebovat!
 //		AI_Output(other,self,"DIA_Fingers_Learnt_15_05"); //How am I supposed to do that?
 //		AI_Output(other,self,"DIA_Fingers_Learnt_15_05"); //Wie soll ich das machen?
-		AI_Output(other,self,"DIA_Fingers_Learnt_15_05"); //Co bych měl udělat?
+		AI_Output(other,self,"DIA_Fingers_Learnt_15_05"); //Jak bych to měl udělat?
 //		AI_Output(self,other,"DIA_Fingers_Learnt_05_06"); //Learn a thief's skill! You need to become more skilled in sneaking around, pickpocketing or opening locks. Then you'll have my vote.
 //		AI_Output(self,other,"DIA_Fingers_Learnt_05_06"); //Lerne ein Diebestalent! Werde geschickter im Schleichen, im Taschendiebstahl oder im Öffnen von Schlössern. Dann werde ich dich dir meine Stimme geben.
 		AI_Output(self,other,"DIA_Fingers_Learnt_05_06"); //Nauč se zlodějskému řemeslu! Potřebuješ se vypracovat v plížení, kapsářství nebo otevírání zámků. Pak dostaneš můj hlas.
@@ -381,7 +381,7 @@ func void DIA_Fingers_Learnt_Info()
 
 //		B_LogEntry(CH1_JoinOC,"Fingers will speak in my favor of me when I learn one of the thieving skills.");
 //		B_LogEntry(CH1_JoinOC,"Fingers wird ein gutes Wort für mich einlegen, wenn ich ein Diebestalent lerne.");
-		B_LogEntry(CH1_JoinOC,"Jakmile získám zlodějskou dovednost, Fingers se za mě přimluví.");
+		B_LogEntry(CH1_JoinOC,"Jakmile se naučím nějakou zlodějskou dovednost, Fingers se za mě přimluví.");
 	};
 };
 

7d8b5264ce0fcf1411210dd779f6bc007753df98
Auronen <45665432+auronen@users.noreply.github.com>
2020-12-16 01:30:35 +0100

    Update DIA_TPL_1455_GorBoba.d
    Korektura, added B_LogEntry

--- content/Story/Missions/DIA_TPL_1455_GorBoba.d
+++ content/Story/Missions/DIA_TPL_1455_GorBoba.d
@@ -46,22 +46,27 @@ func void Info_TPL_1455_GorBoba1_Info()
 	AI_Output(other,self,"Info_TPL_1455_GorBoba1_15_01"); //A TY mě chceš zastavit?
 //	AI_Output(self,other,"Info_TPL_1455_GorBoba1_08_02"); //YES! I will stop you - you won't prevent Master Kalom from awakening the Sleeper.
 //	AI_Output(self,other,"Info_TPL_1455_GorBoba1_08_02"); //JA! Ich werde dich aufhalten, damit du Meister Kalom nicht von seinem Vorhaben abhalten kannst, den Schläfer zu erwecken.
-	AI_Output(self,other,"Info_TPL_1455_GorBoba1_08_02"); //ANO! Zastavím tě - nebudeš rušit mistra Kaloma při probouzení Spáče.
+	AI_Output(self,other,"Info_TPL_1455_GorBoba1_08_02"); //ANO! Zastavím tě - bezabráníš mistru Kalomovi v probouzení Spáče.
 //	AI_Output(self,other,"Info_TPL_1455_GorBoba1_08_03"); //Don't try to attack me, the high priest has transferred part of his powers to Master Kalom and us. Now we are immortal.
 //	AI_Output(self,other,"Info_TPL_1455_GorBoba1_08_03"); //Versuche nicht, mich anzugreifen, der Hohepriester hat Meister Kalom und uns anderen einen Teil seiner Macht verliehen, wir sind nun unsterblich.
-	AI_Output(self,other,"Info_TPL_1455_GorBoba1_08_03"); //Nepokoušej se mě napadnout, nejvyšší kněz přenesl část svých sil na mistra Kaloma a na nás. Jsme teď nesmrtelní.
+	AI_Output(self,other,"Info_TPL_1455_GorBoba1_08_03"); //Nepokoušej se mě napadnout, nejvyšší kněz na nás a msitra Kaloma přenesl část svých sil. Jsme teď nesmrtelní.
 //	AI_Output(other,self,"Info_TPL_1455_GorBoba1_15_04"); //I'll show you how immortal you are!
 //	AI_Output(other,self,"Info_TPL_1455_GorBoba1_15_04"); //Ich kann dir zeigen, wie unsterblich du bist!
-	AI_Output(other,self,"Info_TPL_1455_GorBoba1_15_04"); //Ukážu ti, jak jsi nesmrtelný!
+	AI_Output(other,self,"Info_TPL_1455_GorBoba1_15_04"); //Můžu ti ukázat, jak jsi nesmrtelný!
 //	AI_Output(self,other,"Info_TPL_1455_GorBoba1_08_05"); //Prepare to meet your Creator!
 //	AI_Output(self,other,"Info_TPL_1455_GorBoba1_08_05"); //Mache dich bereit, deinem Schöpfer gegenüber zu treten.
-	AI_Output(self,other,"Info_TPL_1455_GorBoba1_08_05"); //Připrav se na setkání se Stvořitelem!
+	AI_Output(self,other,"Info_TPL_1455_GorBoba1_08_05"); //Připrav se na setkání se stvořitelem!
 
 	AI_StopProcessInfos(self);
 
-	Log_CreateTopic        ( "Orktempel",LOG_NOTE);
-	B_LogEntry            ( "Orktempel","GorBoba wanted to stop me. He told me that Kalom and his supporters had been given powers by the high priest to make them invincible by human weapons."); 
+//	Log_CreateTopic        ( "Orktempel",LOG_NOTE);
+//	B_LogEntry            ( "Orktempel","GorBoba wanted to stop me. He told me that Kalom and his supporters had been given powers by the high priest to make them invincible by human weapons."); 
+	
+	Log_CreateTopic       ( "Skřetí chrám",LOG_NOTE);
+	B_LogEntry            ( "Skřetí chrám","GorBoba mě chěl zastavit. Řekl mi, že nejvyšší kněz poskytl Kalomovi a jeho přívržencům dar nesmrtelnost, nemohou být poškozeni normálnimi zrbaněmi."); 
 
+	
+	
 	Npc_SetAttitude(self,ATT_HOSTILE);
 	Npc_SetTempAttitude(self,ATT_HOSTILE); 
 };

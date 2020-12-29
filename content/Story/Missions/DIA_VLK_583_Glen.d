2ebf03d7c5dbf14ee3b7c1e8da89349613ead63f
Auronen <45665432+auronen@users.noreply.github.com>
2020-12-29 15:26:51 +0100

    Update DIA_VLK_583_Glen.d
    Korektura

--- content/Story/Missions/DIA_VLK_583_Glen.d
+++ content/Story/Missions/DIA_VLK_583_Glen.d
@@ -47,16 +47,16 @@ func void VLK_583_Glen_INFO_Info()
 	AI_Output(other,self,"VLK_583_Glen_INFO_Info_15_01"); //Na co bych měl pamatovat, až budu v dole?
 //	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_02"); //Beware of the minecrawlers.
 //	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_02"); //Auf die Minecrawler.
-	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_02"); //Dávat si pozor na důlní červy.
+	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_02"); //Na důlní červy.
 //	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_03"); //The further you get down, the closer you get to the beasts. Those rotten creatures come directly from Beliar's realm!
 //	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_03"); //Je weiter du nach unten kommst, desto näher bist den Viechern. Die Mistdinger kommen direkt aus Beliars Reich!
-	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_03"); //Čím jdeš níž, tím blíž se k těm bestiím dostáváš. Ty prašivé příšery přicházejí rovnou z Beliarova království!
+	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_03"); //Čím jdeš níž, tím blíž se k těm bestiím dostáváš. Ty prašivé bestie přicházejí rovnou z Beliarovy říše!
 //	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_04"); //You can hear them scraping and scratching. They are sitting in the dark, just waiting for you. I've seen them kill...
 //	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_04"); //Man kann sie schaben und kratzen hören. Sie lauern in der Dunkelheit und warten nur auf dich. Ich habe gesehen, wie sie getötet haben.
-	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_04"); //Uslyšíš hrabání a škrábání. Drží se v temnotě a vyčkávají na tebe. Viděl jsem je zabíjet...
+	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_04"); //Je slyšet jak hrabou a škrábou. Číhají v temnotě a vyčkávají na tebe. Viděl jsem je zabíjet.
 //	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_05"); //They're merciless and fast. If you enjoy being alive, you shouldn't be down here. You better go before something happens to you.
 //	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_05"); //Gnadenlos und schnell. Wenn du am Leben hängst, solltest du dich nicht hier unten aufhalten. Geh lieber, bevor dir noch was passiert.
-	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_05"); //Jsou nemilosrdní a rychlí. Jestli chceš zůstat naživu, neměl bys chodit dolu. Měl bys odejít, než se ti něco stane.
+	AI_Output(self,other,"VLK_583_Glen_INFO_Info_02_05"); //Jsou nemilosrdní a rychlí. Jestli chceš zůstat naživu, neměl bys chodit dolů. Měl bys odejít, než se ti něco stane.
 };  
 // ***************** Infos *****************************
 
@@ -87,7 +87,7 @@ func void VLK_583_Glen_LOCKPICK_Info()
 	AI_Output(other,self,"VLK_583_Glen_LOCKPICK_15_01"); //Slyšel jsem, že bych tu dostal paklíče?
 //	AI_Output(self,other,"VLK_583_Glen_LOCKPICK_02_02"); //Well, since you were brainy enough to find that out, you can have some. 10 ore a piece.
 //	AI_Output(self,other,"VLK_583_Glen_LOCKPICK_02_02"); //Nun, wenn du schlau genug warst, das herauszufinden, dann kannst du welche haben. 10 Erz das Stück.
-	AI_Output(self,other,"VLK_583_Glen_LOCKPICK_02_02"); //Jistě, protože jsi dost bystrý, když jsi to našel, tak můžeš nějaký dostat. 10 nugetů za kus.
+	AI_Output(self,other,"VLK_583_Glen_LOCKPICK_02_02"); //Jistě, protože jsi dost bystrý, když jsi to zjistil, tak si můžeš nějaký koupit. 10 nugetů za kus.
 
 };
 // ***************** Infos *****************************
@@ -101,7 +101,7 @@ instance VLK_583_Glen_BUY(C_INFO)
 	permanent = 1;
 //	description = "(buy lockpicks)";
 //	description = "(Dietriche kaufen)";
-	description = "(kup paklíče)";
+	description = "Chci koupit paklíče."; // #COMMENT Zvyšuje immersion:tm:
 	trade = 1;
 };                       
 

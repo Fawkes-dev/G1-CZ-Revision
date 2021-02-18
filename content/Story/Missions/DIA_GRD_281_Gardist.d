88183b927a8e0d65f6cec98e62959565b69cf1b4
Auronen <45665432+auronen@users.noreply.github.com>
2021-02-18 17:43:12 +0100

    Update DIA_GRD_281_Gardist.d
    Korektura

--- content/Story/Missions/DIA_GRD_281_Gardist.d
+++ content/Story/Missions/DIA_GRD_281_Gardist.d
@@ -52,6 +52,6 @@ func void DIA_Grd_281_GuardGate_Info()
 	AI_Output(other,self,"DIA_Grd_281_GuardGate_15_00"); //Jak to jde?
 //	AI_Output(self,other,"DIA_Grd_281_GuardGate_07_01"); //Good. None of the rogues from the New Camp are to be seen anywhere!
 //	AI_Output(self,other,"DIA_Grd_281_GuardGate_07_01"); //Gut. Weit und breit keiner von den Banditen aus dem Neuen Lager zu sehen!
-	AI_Output(self,other,"DIA_Grd_281_GuardGate_07_01"); //Dobře. Nikdo z té lůzy z Nového tábora se tu neukázal!
+	AI_Output(self,other,"DIA_Grd_281_GuardGate_07_01"); //Dobře. Široko daleko žádný bandita z Nového tábora!
 	AI_StopProcessInfos(self);
 };

9946d50dcf28241f9b181e23590e9c2102077866
Auronen <45665432+auronen@users.noreply.github.com>
2021-02-25 19:00:59 +0100

    Update DIA_GUR_1200_YBerion.d
    Korektura

--- content/Story/Missions/DIA_GUR_1200_YBerion.d
+++ content/Story/Missions/DIA_GUR_1200_YBerion.d
@@ -80,13 +80,13 @@ func void DIA_YBerion_Kennen_Info()
 	AI_Output(other,self,"DIA_YBerion_Kennen_15_00"); //Buď pozdraven, mistře Y´Berione.
 //	AI_Output(self,other,"DIA_YBerion_Kennen_12_01"); //Ah! I know your face!
 //	AI_Output(self,other,"DIA_YBerion_Kennen_12_01"); //Ah! Du kommst mir bekannt vor!
-	AI_Output(self,other,"DIA_YBerion_Kennen_12_01"); //Ach! Tuhle tvář znám!
+	AI_Output(self,other,"DIA_YBerion_Kennen_12_01"); //Ah! Připadáš mi povědomý!
 //	AI_Output(other,self,"DIA_YBerion_Kennen_15_02"); //Impossible. We've never met before.
 //	AI_Output(other,self,"DIA_YBerion_Kennen_15_02"); //Unmöglich. Wir haben uns noch nie gesehen.
-	AI_Output(other,self,"DIA_YBerion_Kennen_15_02"); //Vyloučeno. Nikdy jsme se neviděli.
+	AI_Output(other,self,"DIA_YBerion_Kennen_15_02"); //Vyloučeno. Ještě nikdy jsme se neviděli.
 //	AI_Output(self,other,"DIA_YBerion_Kennen_12_03"); //But I thought... Well - what do you want?
 //	AI_Output(self,other,"DIA_YBerion_Kennen_12_03"); //Und ich dachte ... Nun gut - was willst du?
-	AI_Output(self,other,"DIA_YBerion_Kennen_12_03"); //Ale přece... Dobře - co chceš?
+	AI_Output(self,other,"DIA_YBerion_Kennen_12_03"); //Ale já myslel... Dobře - co chceš?
 };
 
 //############################### KAPITEL 2 ###############################
@@ -127,7 +127,7 @@ func void Info_YBerion_BringFocus_Info()
 	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_Info_12_03"); //Poslali jsme novice Nyrase, aby ho přinesl. Ještě se ale nevrátil.
 //	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_Info_12_04"); //You could check up for me what has happened!
 //	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_Info_12_04"); //Du könntest für mich nachsehen, was passiert ist!
-	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_Info_12_04"); //Mohl bys pro mě vypátrat, co se stalo.
+	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_Info_12_04"); //Mohl bys pro mě zjistit, co se stalo.
 
 	Info_ClearChoices(Info_YBerion_BringFocus);
 	Info_AddChoice(Info_YBerion_BringFocus,DIALOG_BACK , Info_YBerion_BringFocus_BACK);
@@ -170,7 +170,7 @@ func void Info_YBerion_BringFocus_WO()
 	AI_Output(other,self,"Sit_2_PSI_Yberion_BringFocus_WO_15_01"); //Kde mám začít pátrat?
 //	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_WO_12_02"); //Leave the Camp, turn right and go up to the tall cliff. Go beyond the forest until you can see the sea. There you'll find the focus.
 //	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_WO_12_02"); //Wenn du das Lager verlässt, wende dich nach rechts und steige hinauf zur großen Klippe. Dort, hinter dem Wald, wo du das Meer sehen kannst, wirst du den Fokus finden.
-	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_WO_12_02"); //Vyjdi z tábora, dej se doprava a pak nahoru na vysoký útes. Pak pokračuj lesem, dokud neuvidíš moře. Tam to ohnisko najdeš.
+	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_WO_12_02"); //Vyjdi z tábora, dej se doprava a pak nahoru na vysoký útes. Tam, za lesem až uvidíš moře, najdeš ohnisko.
 };
 
 func void Info_YBerion_BringFocus_FOKUS()
@@ -180,19 +180,19 @@ func void Info_YBerion_BringFocus_FOKUS()
 	AI_Output(other,self,"Sit_2_PSI_Yberion_BringFocus_FOKUS_15_01"); //Proč je to ohnisko tak důležité?
 //	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_02"); //It is a magical object which can concentrate our spiritual powers.
 //	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_02"); //Es ist ein magischer Gegenstand, der unsere geistigen Kräfte vereinigen wird.
-	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_02"); //Je to magický předmět, který soustřeďuje naši duchovní sílu.
+	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_02"); //Je to magický předmět, který soustředí naši duchovní sílu.
 //	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_03"); //It is one of the five focus stones used by the mages to erect the Barrier.
 //	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_03"); //Es handelt sich dabei um einen der fünf Fokussteine, den die Magier zum Errichten der Barriere benutzt haben.
 	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_03"); //Je to jeden z pěti ohniskových kamenů, které použili mágové ke vztyčení Bariéry.
 //	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_04"); //In a vision I saw that we need to use the power of the focus.
 //	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_04"); //In einer Vision sah ich, dass wir uns die Kraft eines Fokus zu Nutze machen müssen.
-	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_04"); //V jedné vidině jsem viděl, jak využíváme sílu toho ohniska.
+	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_04"); //V jedné vidině jsem viděl, že musíme využít sílu toho ohniska.
 //	AI_Output(other,self,"Sit_2_PSI_Yberion_BringFocus_FOKUS_15_05"); //Sounds like quite an interesting vision.
 //	AI_Output(other,self,"Sit_2_PSI_Yberion_BringFocus_FOKUS_15_05"); //War wohl eine ziemlich spannende Vision.
 	AI_Output(other,self,"Sit_2_PSI_Yberion_BringFocus_FOKUS_15_05"); //To byla docela zajímavá vidina.
 //	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_06"); //My vision was a sign from the Sleeper. We can reach him with the focus!
 //	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_06"); //Meine Vision war ein Zeichen vom Schläfer. Mit dem Fokus können wir ihn erreichen!
-	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_06"); //Moje vidina byla znamením od Spáče. Můžeme k němu dojít pomocí toho ohniska!
+	AI_Output(self,other,"Sit_2_PSI_Yberion_BringFocus_FOKUS_12_06"); //Moje vidina byla znamením od Spáče. Pomocí ohniska se s ním budeme moci spojit!
 };
 
 // ------------------------------ RUNNING ----------------------------------
@@ -223,7 +223,7 @@ func void Info_YBerion_BringFocus_RUNNING_Info()
 	AI_Output(other,self,"Info_YBerion_BringFocus_RUNNING_15_01"); //Pořád to ohnisko nemůžu najít.
 //	AI_Output(self,other,"Info_YBerion_BringFocus_RUNNING_12_02"); //Then search on. The Sleeper will guide the way.
 //	AI_Output(self,other,"Info_YBerion_BringFocus_RUNNING_12_02"); //Dann suche weiter. Der Schläfer wird dir den Weg weisen.
-	AI_Output(self,other,"Info_YBerion_BringFocus_RUNNING_12_02"); //Tak hledej. Spáč tě povede.
+	AI_Output(self,other,"Info_YBerion_BringFocus_RUNNING_12_02"); //Tak hledej. Spáč ti ukáže cestu.
 };
 
 // ------------------------------ SUCCESS ----------------------------------
@@ -254,21 +254,21 @@ func void Info_YBerion_BringFocus_Success_Info()
 	AI_Output(other,self,"Info_YBerion_BringFocus_Success_15_01"); //Našel jsem to ohnisko.
 //	AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_02"); //Excellent. You have done us a great favor.
 //	AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_02"); //Hervorragend. Du hast uns einen großen Dienst erwiesen.
-	AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_02"); //Výborně. Nesmírně jsi nám pomohl.
+	AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_02"); //Výborně. Prokázal jsi nám velikou službu.
 //	AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_03"); //Now take the focus back to Cor Kalom. He'll know what needs to be done.
 //	AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_03"); //Bring nun den Fokus zu Cor Kalom. Er wird wissen, was zu tun ist.
-	AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_03"); //Teď dej to ohnisko zpátky Corovi Kalomovi. Ten ví, co je třeba udělat.
+	AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_03"); //Dones to ohnisko zpátky Cor Kalomovi. Ten bude vědět, co je třeba udělat.
 	if (Kalom_TalkedTo == FALSE)
 	{
 //		AI_Output(other,self,"Info_YBerion_BringFocus_Success_15_04"); //Where can I find this Cor Kalom?
 //		AI_Output(other,self,"Info_YBerion_BringFocus_Success_15_04"); //Wo kann ich diesen Cor Kalom finden?
-		AI_Output(other,self,"Info_YBerion_BringFocus_Success_15_04"); //Kde najdu toho Cora Kaloma?
+		AI_Output(other,self,"Info_YBerion_BringFocus_Success_15_04"); //Kde najdu toho Cor Kaloma?
 //		AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_05"); //He spends most of his time in the alchemy lab in the upper section of the catwalks.
 //		AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_05"); //Er hält sich die meiste Zeit im Alchemielabor auf der oberen Stegebene auf.
-		AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_05"); //Většinu času tráví ve své alchymistické dílně v horní sekci můstků.
+		AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_05"); //Většinu času tráví ve své alchymistické dílně v horní úrovni můstků.
 //		AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_06"); //Talk to Lester at the entrance to the Camp... he often guides newcomers around the Camp.
 //		AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_06"); //Frag Lester am Eingang des Lagers ... Er führt öfter Neulinge herum.
-		AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_06"); //Zeptej se u vchodu do tábora Lestera... ten často provádí nováčky po táboru.
+		AI_Output(self,other,"Info_YBerion_BringFocus_Success_12_06"); //Zeptej se u vchodu do tábora Lestera... Často tam doprovází nováčky.
 //		B_LogEntry(CH2_Focus,"Y'Berion told me to take the focus to another guru called Cor Kalom. This Cor Kalom spends the whole day in the alchemy lab."); 
 //		B_LogEntry(CH2_Focus,"Y'Berion hat mich beauftragt, den Fokus zu einem anderen Guru namens 'Cor Kalom' zu bringen. Dieser Cor Kalom soll sich den ganzen Tag im Alchemielabor aufhalten."); 
 		B_LogEntry(CH2_Focus,"Y´Berion mi řekl, abych to ohnisko předal dalšímu guru jménem Cor Kalom. Cor Kalom tráví celé dny ve své alchymistické dílně."); 
@@ -277,7 +277,7 @@ func void Info_YBerion_BringFocus_Success_Info()
 	{
 //		B_LogEntry(CH2_Focus,"Y'Berion told me to take the focus to Cor Kalom. What a pleasure to meet this very charming person again.");
 //		B_LogEntry(CH2_Focus,"Y'Berion hat mich beauftragt, den Fokus zu Cor Kalom zu bringen. Welch eine 'Freude' diesen überaus liebenswürdigen Menschen wieder zu treffen.");
-		B_LogEntry(CH2_Focus,"Y´Berion mi řekl, abych to ohnisko předal  Cor Kalomovi. Jaké potěšení, shledat se zase s tím příjemným chlapíkem.");
+		B_LogEntry(CH2_Focus,"Y'Berion mi řekl, abych to ohnisko předal Cor Kalomovi. Jaké potěšení, shledat se zase s tím příjemným chlapíkem.");
 	};
 
 	Tpl_1406_Templer.aivar[AIV_PASSGATE] = TRUE; //Türwache vor dem Labor
@@ -314,13 +314,13 @@ func void Info_YBerion_NYRAS_Info()
 	AI_Output(other,self,"Info_YBerion_NYRAS_15_01"); //Novic Nyras zešílel!
 //	AI_Output(other,self,"Info_YBerion_NYRAS_15_02"); //He said that the Sleeper had talked to him and chosen him as his only servant!
 //	AI_Output(other,self,"Info_YBerion_NYRAS_15_02"); //Er faselte davon, dass der Schläfer zu ihm gesprochen habe und ihn als einzigen Diener auserwählte!
-	AI_Output(other,self,"Info_YBerion_NYRAS_15_02"); //Říkal, že mluvil se Spáčem, který si ho zvolil za svého pomocníka!
+	AI_Output(other,self,"Info_YBerion_NYRAS_15_02"); //Říkal, že mluvil se Spáčem, který si ho zvolil za svého služebníka!
 //	AI_Output(other,self,"Info_YBerion_NYRAS_15_03"); //Then he attacked and tried to kill me!
 //	AI_Output(other,self,"Info_YBerion_NYRAS_15_03"); //Dann griff er mich sogar an und wollte mich töten!
 	AI_Output(other,self,"Info_YBerion_NYRAS_15_03"); //Pak mě napadl a pokusil se mě zabít!
 //	AI_Output(self,other,"Info_YBerion_NYRAS_12_04"); //The might of the Sleeper is so vast that unprepared souls despair over it.
 //	AI_Output(self,other,"Info_YBerion_NYRAS_12_04"); //Die Macht des Schläfers ist so gewaltig, dass sie unvorbereitete Seelen in Verwirrung stürzen kann.
-	AI_Output(self,other,"Info_YBerion_NYRAS_12_04"); //Spáčova moc je tak nesmírná, že nepřipravené duše přivede k zoufalství!
+	AI_Output(self,other,"Info_YBerion_NYRAS_12_04"); //Spáčova moc je tak nesmírná, že může uvrhnout nepřipravené duše do zoufalství! #Needs_attention tady je to kostrbaté, ale opsal jsem to z nějakého náboženského textu :D
 //	AI_Output(self,other,"Info_YBerion_NYRAS_12_05"); //Only we Gurus, in our years of meditation, have achieved the spiritual strength to bear the voice of the almighty Sleeper!
 //	AI_Output(self,other,"Info_YBerion_NYRAS_12_05"); //Nur wir Gurus haben durch jahrelanges Meditieren die seelische Stärke gewonnen, die Stimme des allmächtigen Schläfers zu ertragen!
 	AI_Output(self,other,"Info_YBerion_NYRAS_12_05"); //Jenom my, Guru, jsme léty strávenými v meditacích dosáhli takové duchovní síly, která dokáže unést sílu hlasu všemohoucího Spáče!
@@ -336,7 +336,7 @@ instance GUR_1200_Yberion_EARN(C_INFO)
 	permanent = 0;
 //	description = "What about some kind of reward?"; 
 //	description = "Wie siehts mit einer Belohnung aus?"; 
-	description = "A co takhle nějaká odměna?"; 
+	description = "Jak to vypadá s mou odměnou?"; 
 };
 
 func int GUR_1200_Yberion_EARN_Condition()
@@ -351,13 +351,13 @@ func void GUR_1200_Yberion_EARN_Info()
 {
 //	AI_Output(other,self,"GUR_1200_Yberion_EARN_Info_15_01"); //What about some kind of reward?
 //	AI_Output(other,self,"GUR_1200_Yberion_EARN_Info_15_01"); //Wie sieht's mit einer Belohnung aus?
-	AI_Output(other,self,"GUR_1200_Yberion_EARN_Info_15_01"); //A co takhle nějakou odměnu?
+	AI_Output(other,self,"GUR_1200_Yberion_EARN_Info_15_01"); //Jak to vypadá s mou odměnou?
 //	AI_Output(self,other,"GUR_1200_Yberion_EARN_Info_12_02"); //Since you're not a member of our Brotherhood, I will reward you.
 //	AI_Output(self,other,"GUR_1200_Yberion_EARN_Info_12_02"); //Da du kein Bruder unserer Gemeinschaft bist, werde ich dich entlohnen.
 	AI_Output(self,other,"GUR_1200_Yberion_EARN_Info_12_02"); //Protože nejsi členem našeho Bratrstva, odměním tě.
 //	AI_Output(self,other,"GUR_1200_Yberion_EARN_Info_12_03"); //Take this amulet as a sign of my gratitude.
 //	AI_Output(self,other,"GUR_1200_Yberion_EARN_Info_12_03"); //Nimm dieses Amulett als Zeichen meiner Dankbarkeit.
-	AI_Output(self,other,"GUR_1200_Yberion_EARN_Info_12_03"); //Vezmi si tento amulet na znamení mé vděčnosti.
+	AI_Output(self,other,"GUR_1200_Yberion_EARN_Info_12_03"); //Vezmi si tento amulet jako znamení mé vděčnosti.
 	CreateInvItem(self,Schutzamulett_Feuer);
 	B_GiveInvItems(self,hero,Schutzamulett_Feuer,1);
 };  

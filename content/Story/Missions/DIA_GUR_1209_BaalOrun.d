841752f710f5b7d7402c0cbeef81eaa166ac2ed7
Fawkes <68402191+Fawkes-dev@users.noreply.github.com>
2021-04-30 01:34:34 +0200

    Update DIA_GUR_1209_BaalOrun.d

--- content/Story/Missions/DIA_GUR_1209_BaalOrun.d
+++ content/Story/Missions/DIA_GUR_1209_BaalOrun.d
@@ -127,10 +127,10 @@ func void DIA_BaalOrun_FirstTalk_Info()
 	AI_Output(self,other,"DIA_BaalOrun_FirstTalk_12_00"); //Mluvil jsem s Ghorimem. Zastal jsi se jednoho z našich bratrů - a to od tebe bylo správné.
 //	AI_Output(self,other,"DIA_BaalOrun_FirstTalk_12_01"); //That's why I've chosen you for a special task.
 //	AI_Output(self,other,"DIA_BaalOrun_FirstTalk_12_01"); //Darum habe ich dich für eine besondere Aufgabe erwählt.
-	AI_Output(self,other,"DIA_BaalOrun_FirstTalk_12_01"); //Proto jsem si tě vybral pro zvláštní úkol.
+	AI_Output(self,other,"DIA_BaalOrun_FirstTalk_12_01"); //Proto jsem si tě vybral pro speciální úkol.
 //	AI_Output(self,other,"DIA_BaalOrun_FirstTalk_12_02"); //Cor Kalom urgently needs new swampweed for his experiments.
 //	AI_Output(self,other,"DIA_BaalOrun_FirstTalk_12_02"); //Cor Kalom braucht dringend neues Sumpfkraut für seine Experimente.
-	AI_Output(self,other,"DIA_BaalOrun_FirstTalk_12_02"); //Col Kalom naléhavě potřebuje novou drogu z bažin pro své experimenty.
+	AI_Output(self,other,"DIA_BaalOrun_FirstTalk_12_02"); //Col Kalom naléhavě potřebuje novou trávu z bažin pro své experimenty.
 //	AI_Output(self,other,"DIA_BaalOrun_FirstTalk_12_03"); //Our gatherers are working day and night. Go to them and take their entire harvest to Kalom's alchemy lab.
 //	AI_Output(self,other,"DIA_BaalOrun_FirstTalk_12_03"); //Unsere Sammler im Sumpf arbeiten Tag und Nacht. Gehe zu ihnen und bring ihre gesamte Ernte zu Kalom ins Alchemielabor.
 	AI_Output(self,other,"DIA_BaalOrun_FirstTalk_12_03"); //Naši sběrači pracují ve dne v noci. Jdi za nimi a přines celou jejich sklizeň Kalomovi do alchymistické dílny.
@@ -145,7 +145,7 @@ func void DIA_BaalOrun_FirstTalk_Info()
 	Log_SetTopicStatus(CH1_DeliverWeed,LOG_RUNNING);
 //	B_LogEntry(CH1_DeliverWeed,"My powers of persuasion with Harlok seem to have impressed the Guru Baal Orun. I now have the honor of taking the entire weed from the novices in the swamp to Cor Kalom.");
 //	B_LogEntry(CH1_DeliverWeed,"Meine Überredungskünste bei Harlok, scheinen den Guru Baal Orun beeindruckt zu haben. Ich haben nun die 'Ehre' das gesammelte Kraut der Novizen im Sumpf zu Cor Kalom zu bringen.");
-	B_LogEntry(CH1_DeliverWeed,"Dovednost přemlouvání v té věci s Harlokem udělala dojem na Baala Oruna. Nyní mám tu čest vybrat pro Cor Kaloma všechnu drogu od noviců v bažinách.");
+	B_LogEntry(CH1_DeliverWeed,"Dovednost 'přemlouvání' v té věci s Harlokem udělala dojem na Baal Oruna. Nyní mám tu čest vybrat pro Cor Kaloma všechnu trávu od noviců v bažinách.");
 	BaalOrun_FetchWeed = LOG_RUNNING;
 
 	Info_ClearChoices(DIA_BaalOrun_FirstTalk);
@@ -221,6 +221,7 @@ func void DIA_BaalOrun_GotWeed_Info()
 	{
 		Log_SetTopicStatus(CH1_JoinPsi,LOG_RUNNING);
 	};
+
 //	B_LogEntry(CH1_JoinPsi,"Baal Orun called me a worthy servant of the Sleeper, because I've fought the bloodflies on behalf of the other novices in the swamp.");
 //	B_LogEntry(CH1_JoinPsi,"Baal Orun bezeichnete mich als würdigen Diener des Schläfers, da ich für die anderen Novizen im Sumpf gegen die Blutfliegen gekämpft habe.");
 	B_LogEntry(CH1_JoinPsi,"Baal Orun mě nazval věrným služebníkem Spáče, protože jsem kvůli novicům z bažin vymýtil krvavé mouchy.");
@@ -261,7 +262,7 @@ func void DIA_BaalOrun_WeedAtKaloms_Info()
 	AI_Output(self,other,"DIA_BaalOrun_WeedAtKaloms_12_01"); //Dobře jsi to provedl. Za tvé úsilí ti dám malou odměnu. Tady je, vem si ji.
 //	AI_Output(self,other,"DIA_BaalOrun_WeedAtKaloms_12_02"); //It's a magic sleep spell. You can only use it once, but I hope it will be of good service to you.
 //	AI_Output(self,other,"DIA_BaalOrun_WeedAtKaloms_12_02"); //Es ist ein magischer Schlafzauber. Du kannst ihn nur einmal verwenden, aber er wird dir hoffentlich gute Dienste leisten.
-	AI_Output(self,other,"DIA_BaalOrun_WeedAtKaloms_12_02"); //To je magické zaříkávání ke spánku. Můžeš je použít jen jedenkrát, ale věřím, že ti prokáže dobrou službu.
+	AI_Output(self,other,"DIA_BaalOrun_WeedAtKaloms_12_02"); //To je magický svitek spánku. Můžeš je použít jen jedenkrát, ale věřím, že ti prokáže dobrou službu.
 
 	BaalOrun_Ansprechbar = TRUE; //damit NoTalk-info nicht kommt
 	B_GiveXP(XP_ReportToBaalOrun);
@@ -301,7 +302,7 @@ func void DIA_BaalOrun_Perm_Info()
 	AI_Output(other,self,"DIA_BaalOrun_Perm_15_00"); //Jak jde produkce drogy z bažin?
 //	AI_Output(self,other,"DIA_BaalOrun_Perm_12_01"); //We're putting maximum strain on every man to produce enough for ourselves AND trade with the other camps.
 //	AI_Output(self,other,"DIA_BaalOrun_Perm_12_01"); //Wir belasten jeden Mann bis ans Äußerste, um genug für uns UND den Handel mit den anderen Lagern herzustellen.
-	AI_Output(self,other,"DIA_BaalOrun_Perm_12_01"); //Zatěžujeme každého muže, aby vyprodukoval dostatek pro nás samotné a na obchodování s ostatními tábory.
+	AI_Output(self,other,"DIA_BaalOrun_Perm_12_01"); //Zatěžujeme každého muže, aby vyprodukoval dostatek pro nás samotné i na obchodování s ostatními tábory.
 //	AI_Output(self,other,"DIA_BaalOrun_Perm_12_02"); //But it's a sacrifice for the Sleeper, one our novices willingly make.
 //	AI_Output(self,other,"DIA_BaalOrun_Perm_12_02"); //Aber es ist ein Opfer für den Schläfer, und so bringen es die Novizen gerne.
 	AI_Output(self,other,"DIA_BaalOrun_Perm_12_02"); //To je ale oběť pro Spáče, kterou naši novicové ochotně přináší.
